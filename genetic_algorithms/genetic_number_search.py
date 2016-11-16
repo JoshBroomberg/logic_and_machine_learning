c65a37f471cfd1911070f4a2cae2dec32ff0203c
JoshBroomberg <joshbroomberg@gmail.com>
2016-11-15 18:38:27 -0800

    genetic algorithms

---
+++ genetic_algorithms/genetic_number_search.py
@@ -0,0 +1,96 @@
+import random
+import math
+from genetic_support import Chromosome
+
+### OPTIONS ###
+target_value = 42
+
+starting_population = 100
+# Breeding can be between 0 and 1 inclusive.
+breeding_proportion = 0.5
+
+# Kill proportion. Controls how many of the inviable solutions die per generation
+# Killing is off by default
+kill_chance = 0
+
+# Mutation chance can be between 0.000 and 1. 0.001 recommended.
+mutation_chance = 0.01
+
+generations_limit = 1000
+#### END OPTIONS ####
+
+population = [Chromosome(None) for _ in range(starting_population)]
+
+def reset():
+  global population
+  population = [Chromosome(None) for _ in range(starting_population)]
+# Breed two chromosomes by selecting a random pivot index
+# and swapping the genes after that pivot forming
+# two new offspring.
+def breed(chrom1, chrom2):
+  # select a pivot that ensures at least one base swaps
+  pivot = random.randint(1, (len(chrom1.genome)-2))
+  new_chrom1 = chrom1.genome[:pivot] + chrom2.genome[pivot:]
+  new_corom2 = chrom2.genome[:pivot] + chrom1.genome[pivot:]
+  return (Chromosome(new_chrom1), Chromosome(new_corom2))
+
+# Mutate a chromosome by choosing a random location
+# and flipping the gene at that location.
+def mutate(chrom):
+  mutation_location = random.randint(0, (len(chrom.genome)-1))
+  bases = list(chrom.genome)
+  
+  # Flip the base
+  bases[mutation_location] = str((int(bases[mutation_location])-1)**2)
+  return Chromosome("".join(bases))
+
+# Determine if a chromosome matches the desired value.
+def eval_population():
+  for chromosome in population:
+    if chromosome.value() == target_value:
+      return chromosome
+  return False
+
+# Counter for generations that have passed.
+generations = 0 
+
+# Loop until a solution is found, or the generation limit is reached, or 
+# the population dies out.
+while not eval_population() and (generations < generations_limit) and (len(population) > 0):
+  print "generation: ", generations + 1, "population: ", len(population)
+  
+  # Kill some chromosomes, if configured to do so.
+  if kill_chance > 0:
+    population = [x for x in population if (not x.is_viable()) and (random.randint(0, 1000)/1000.0 < kill_chance)]
+
+  # Shuffle chromosome order to randomise breeding.
+  random.shuffle(population)
+  
+  # Breed the number of pairs based on breeding proportion.
+  num_breeding_pairs = int((len(population)/2)*breeding_proportion)
+
+  for index in range(0, num_breeding_pairs, 2):
+    (new_crom1, new_crom2) = breed(population[index], population[index+1])
+    population[index] = new_crom1
+    population[index+1] = new_crom2
+
+  # Mutate some chromosomes based on chance.
+  for index, chromosome in enumerate(population):
+    if random.randint(0, 1000)/1000.0 < mutation_chance:
+      population[index] = mutate(chromosome)
+
+  generations += 1
+
+# Display results. 
+print ""
+print "######"
+
+if eval_population():
+  print "solution found!"
+  print "Genome: ", eval_population().genome
+  print "Decoded genome: ", eval_population().decode_genome(), "=", target_value
+  print "Generations taken: ", generations
+elif generations >= generations_limit: 
+  print "Generations limit exceeded: ", generations_limit
+elif len(population) == 0:
+  print "population went extinct! Try upping your size, or decreasing kill chance."
---
+++ genetic_algorithms/genetic_support.py
@@ -0,0 +1,105 @@
+import numpy as np
+import textwrap
+
+class Chromosome:
+  gene_dict = {
+    "0001": "1",
+    "0010": "2",
+    "0011": "3",
+    "0100": "4",
+    "0101": "5",
+    "0110": "6",
+    "0111": "7",
+    "1000": "8",
+    "1001": "9",
+    "1010": "+",
+    "1011": "-",
+    "1100": "*",
+    "1101": "/",
+  }
+
+  def __init__(self, genome):
+    if genome:
+      self.genome = genome
+    else:
+      self.genome = Chromosome.random_genome()
+
+  # Return a string version of the binary genome as numbers/operators
+  def decode_genome(self):
+    # Split the genome into 4 genes
+    genes = textwrap.wrap(self.genome, 4)
+    
+    # decode the binary genes into string form.
+    decoded_genes = [Chromosome.lookup_gene(gene) for gene in genes]
+    # Validate the genome
+    if not Chromosome.validate_genome(decoded_genes):
+      return None
+    
+    # If valid, join the genome together and return
+    return "".join(decoded_genes)
+
+  # Boolean value which represents if the genome is viable for evaluation.
+  def is_viable(self):
+    # note: "not not" just converts truthy/falsey value to True/False
+    return not not self.decode_genome()
+
+  # Return the value of the chromosome.
+  def value(self):
+    if not self.is_viable():
+      return None
+    else:
+      return eval(self.decode_genome())
+
+
+  ### STATIC UTILITY METHODS ###
+
+  # Create a random 12 digit binary string
+  @staticmethod
+  def random_genome():
+    return ''.join([str(x) for x in np.random.randint(2, size=(20,))])
+
+  # Converts binary gene to literal version.
+  @staticmethod
+  def lookup_gene(gene):
+    try:
+      return Chromosome.gene_dict[gene]
+    except KeyError:
+      return ""
+  
+  # Takes an array of genes.
+  # Validates genome is mathematically operable.
+  @staticmethod
+  def validate_genome(genes):
+    # First gene must be a number
+    number_required = True
+    for gene in genes:
+      is_number = False
+      # Try to convert string value into an int.
+      try:
+        int(gene)
+        # If line above doesn't throw an error, string is number.
+        is_number = True
+      except ValueError:
+        False
+    
+      if number_required and (not is_number):
+        return False
+
+      elif (not number_required) and is_number:
+        return False
+
+      elif gene == genes[-2]:
+        number_required = True
+      
+      elif gene == "":
+        continue
+
+      elif number_required and is_number:
+        # next run, number is not required.
+        number_required = False
+      elif (not number_required) and (not is_number):
+        # next run, number is required.
+        number_required = True
+
+    # Return true if loop completes without failure.
+    return True
